4ad34230d69c2963b7800a688897230bdc753cb3
lizhengxin <1558268927@qq.com>
2023-06-25 18:35:03 +0800

    连接数据库

--- src/views/children/vidoeWebRTC.vue
+++ src/views/children/vidoeWebRTC.vue
@@ -2,31 +2,44 @@
     <div class="flex">
         
         <button @click="sendMsg">发送信息</button>
+
+        <button @click="getDevice">获取设备信息</button>
+
     </div>
 </template>
 
 
 <script setup lang="ts">
     import { WEBSOCKET_DOMAIN } from '../../utils/public'
+
+    import { CreateRequest } from '../../utils/request'
+
+
     const ws = new WebSocket(WEBSOCKET_DOMAIN);
     ws.onopen = () => {
-      console.log('WebSocket连接成功');
-      // ws.send('test');
+        console.log('WebSocket连接成功');
+        // ws.send('test');
     };
     ws.onerror = (err) => {
-      console.log('WebSocket连接失败:', err);
+        console.log('WebSocket连接失败:', err);
     };
     ws.onclose = (close: any) => {
-      console.log('WebSocket连接关闭:', close);
+        console.log('WebSocket连接关闭:', close);
     };
     ws.onmessage = (event) => {
-      console.log('WebSocket收到信息: ', event);
+        console.log('WebSocket收到信息: ', event);
     };
 
 
 
     function sendMsg(){
         ws.send('发送测试消息');
+    }
+
+    function getDevice(){
+        CreateRequest('GET', '/get/getDeviceInfo').then(res => {
+            console.log(res)
+        })
     }
 </script>
 

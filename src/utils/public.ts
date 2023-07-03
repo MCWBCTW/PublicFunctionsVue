cc988ba15067fe45cdc59e7c2f382e84d744f5d5
lzx <lzx_599599@163.com>
2023-07-03 22:37:33 +0800

    环境判断

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -2,9 +2,19 @@
 /**
  * 定义一些全局使用的公共参数
  */
+let HTTP = '';
+if (process.env.NODE_ENV === 'development') {
+    // 开发环境下的逻辑
+    console.log('当前是开发环境');
+    HTTP = 'http://127.0.0.1:3050'
+} else if (process.env.NODE_ENV === 'production') {
+    // 正式环境下的逻辑
+    console.log('当前是正式环境');
+    HTTP = 'http://functions.mcwbctw.cn:3050'
+}
 
 // http请求域名配置
-export const REQUEST_HTTP = 'http://127.0.0.1:3050';
+export const REQUEST_HTTP = HTTP;
 
 // webSocket 连接域名配置
 export const WEBSOCKET_DOMAIN = 'ws://127.0.0.1:3060';
